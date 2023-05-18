6633d53eab2b8926918d2606414d3f9ee5006a68
Maicol Lazo <maicollazo123@gmail.com>
2023-05-18 10:52:03 -0300

    config-server

--- CheckAgro/config-server/src/test/java/com/dh/configserver/ConfigServerApplicationTests.java
+++ CheckAgro/config-server/src/test/java/com/dh/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.dh.eurekaserver;
+package com.dh.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServerApplicationTests {
+class ConfigServerApplicationTests {
 
     @Test
     void contextLoads() {
