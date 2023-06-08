38770c47bbced431e6a2170683b9f268f91f84c1
Sofia-Planes <sofiabplanes@gmail.com>
2023-06-08 20:05:19 -0300

    Configuracion Mongo completa

--- CheckAgro/api-customer/src/main/java/com/jmg/checkagro/customer/repository/CustomerRepository.java
+++ CheckAgro/api-customer/src/main/java/com/jmg/checkagro/customer/repository/CustomerRepository.java
@@ -1,19 +1,19 @@
 package com.jmg.checkagro.customer.repository;
 
 import com.jmg.checkagro.customer.model.Customer;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
-
 import java.util.Optional;
 
 @Repository
 public interface CustomerRepository extends MongoRepository<Customer,Long> {
     @Transactional
     //@Modifying
-    //@Query("update Customer c set c.active = ?1 where c.id = ?2")
+    @Query("update Customer c set c.active = ?1 where c.id = ?2")
     void updateActive(Boolean active, Long id);
+
 
 
     Optional<Customer> findByDocumentTypeAndDocumentNumber(String documentType, String documentNumber);
