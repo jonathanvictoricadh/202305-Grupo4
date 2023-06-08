15678cda8525800cfb959ac78a8f54bf3494b725
Sofia-Planes <sofiabplanes@gmail.com>
2023-06-07 21:05:21 -0300

    Mongo: cambiamos la api-custumer, pero todavia no funciona

--- CheckAgro/api-customer/src/main/java/com/jmg/checkagro/customer/model/Customer.java
+++ CheckAgro/api-customer/src/main/java/com/jmg/checkagro/customer/model/Customer.java
@@ -1,38 +1,33 @@
 package com.jmg.checkagro.customer.model;
 
+import com.mongodb.client.model.Collation;
 import lombok.*;
 import org.hibernate.annotations.Type;
+import org.hibernate.mapping.Collection;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-@Entity
-@Table(name = "customer")
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @Builder
+@Document(collection = "customer")
+
 public class Customer implements Serializable {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column( nullable = false)
     private Long id;
-    @Column(nullable = false, length = 10)
     private String documentType;
-    @Column(nullable = false, length = 20)
     private String documentNumber;
-    @Column(nullable = false, length = 100)
     private String businessName;
-    @Column(nullable = false, length = 250)
     private String email;
-    @Column(nullable = false, length = 20)
     private String phone;
-    @Column(nullable = false)
     private LocalDate creation;
-    @Column(nullable = false, columnDefinition = "integer default 1")
     @Type(type = "org.hibernate.type.NumericBooleanType")
     private Boolean active;
 
